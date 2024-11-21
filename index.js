8f7133fccee4a959ef648796f1a54ace6c19f296
Daniel Leon <carlosmatusdev@gmail.com>
2024-11-20 23:33:02 -0600

    changed from commojs to modules and changed port used

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
 
+//Routing
 app.get('/', (req, res) => {
     res.send('Hello World from Express!')
-});
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
