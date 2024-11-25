14c61497e229239e5eb2754b1648f493e6cd7199
carlos.matus <carlos.matus@coppel.com>
2024-11-25 17:57:07 -0600

    functionality for creating new user

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,7 +15,8 @@ const userSchema = new Schema({
     email: {
         type: String, 
         required: true,
-        trim: true
+        trim: true,
+        unique:true
     },
     password: {
         type: String, 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import User from './models/User'
 const router = Router()
 
 // Authentication and register
-router.post('/auth/register', (req, res) => {
-    res.send(req.body)
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body)
+    await user.save()
+
+    res.send({
+        msg: "User created successfully"
+    })
 })
 
 export default router
